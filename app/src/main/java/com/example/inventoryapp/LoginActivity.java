6f538a7a5de3f8b36d44507b90dae550e61879b4
Axi20 <varagn19@gmail.com>
2023-01-25 16:47:56 +0100

    Signup activity size

--- app/src/main/java/com/example/inventoryapp/LoginActivity.java
+++ app/src/main/java/com/example/inventoryapp/LoginActivity.java
@@ -1,15 +1,91 @@
 package com.example.inventoryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText login_email, login_password;
+    Button login_button;
+    TextView signupRedirectText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_email = findViewById(R.id.login_email);
+        login_password = findViewById(R.id.login_password);
+        login_button = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+    }
+
+    public Boolean validateEmail(){
+        String val = login_email.getText().toString();
+        if(val.isEmpty()){
+            login_email.setError("Email cannot be empty!");
+            return false;
+        } else {
+            login_email.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatePassword(){
+        String val = login_password.getText().toString();
+        if(val.isEmpty()){
+            login_password.setError("Password cannot be empty!");
+            return false;
+        } else {
+            login_password.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser(){
+        String userEmail = login_email.getText().toString().trim();
+        String userPassword = login_password.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("customers");
+        Query checkUserDatabase = reference.orderByChild("email").equalTo(userEmail);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                login_email.setError(null);
+                String passwordFromDB = snapshot.child(userEmail).child("password").getValue(String.class);
+
+                if(!Objects.equals(passwordFromDB, userPassword)){
+                    login_email.setError(null);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    login_password.setError("Invalid credentials");
+                    login_password.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/inventoryapp/SignupActivity.java
+++ app/src/main/java/com/example/inventoryapp/SignupActivity.java
@@ -1,14 +1,56 @@
 package com.example.inventoryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignupActivity extends AppCompatActivity {
+
+    EditText signup_firstname, signup_lastname, signup_email, signup_password;
+    TextView loginRedirectText;
+    Button signup_button;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signup_firstname = findViewById(R.id.signup_firstname);
+        signup_lastname = findViewById(R.id.signup_lastname);
+        signup_email = findViewById(R.id.signup_email);
+        signup_password = findViewById(R.id.signup_password);
+        signup_button = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signup_button.setOnClickListener(view -> {
+            database = FirebaseDatabase.getInstance();
+            reference = database.getReference("customers");
+            String firstname = signup_firstname.getText().toString();
+            String lastname = signup_lastname.getText().toString();
+            String email = signup_email.getText().toString();
+            String password = signup_password.getText().toString();
+
+            HelperClass helperClass = new HelperClass(firstname, lastname, email, password);
+            reference.child(email).setValue(helperClass);
+
+            Toast.makeText(SignupActivity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        loginRedirectText.setOnClickListener(v -> {
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(intent);
+        } );
     }
 }
